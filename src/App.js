6ee990adf9d00cec82bbcb5232bf288e3b3829db
rohan <rohanrajgautam@gmail.com>
2020-07-01 19:56:13 +0545

    add http request and update state

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
-import React, { Component } from 'react';
-import Navbar from './components/layout/Navbar'
-import Users from './components/users/Users'
-import './App.css';
+import React, { Component } from "react";
+import Navbar from "./components/layout/Navbar";
+import Users from "./components/users/Users";
+import axios from "axios";
+import "./App.css";
 
 class App extends Component {
+  state = {
+    users: [],
+    loading: false,
+  };
+
+  async componentDidMount() {
+    this.setState({ loading: true });
+    const res = await axios.get("https://api.github.com/users");
+    this.setState({ users: res.data, loading: false });
+  }
   render() {
     return (
-      <div className="App" >
+      <div className='App'>
         <Navbar />
-        <div className="container">
-          <Users />
+        <div className='container'>
+          <Users loading={this.state.loading} users={this.state.users} />
         </div>
       </div>
     );
