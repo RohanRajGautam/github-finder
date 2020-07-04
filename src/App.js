3c1eabede6d70bcff9ae88dd3476a4e38b1cce10
rohan <rohanrajgautam@gmail.com>
2020-07-04 08:51:56 +0545

    create home and not found page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import React, { Fragment } from "react";
+import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
-import Users from "./components/users/Users";
 import User from "./components/users/User";
-import Search from "./components/users/Search";
 import Alert from "./components/layout/Alert";
 import About from "./components/pages/About";
 import Repos from "./components/repos/Repos";
+import Home from "./components/pages/Home";
+import NotFound from "./components/pages/NotFound";
 import "./App.css";
 
 import GithubState from "./context/github/GithubState";
@@ -22,18 +22,10 @@ const App = () => {
             <div className='container'>
               <Alert />
               <Switch>
-                <Route
-                  exact
-                  path='/'
-                  render={(props) => (
-                    <Fragment>
-                      <Search />
-                      <Users />
-                    </Fragment>
-                  )}
-                />
+                <Route exact path='/' component={Home} />
                 <Route exact path='/about' component={About} />
                 <Route exact path='/user/:login' component={User} />
+                <Route component={NotFound} />
               </Switch>
             </div>
           </div>
